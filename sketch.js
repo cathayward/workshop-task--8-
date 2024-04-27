0b6a57fd1f4840238e3a0c15d6a02f67a2769e21
cat hayward <161404962+cathayward@users.noreply.github.com>
2024-04-27 18:30:36 +1200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+let searchPath = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=";
+let searchJSON;
+let img;
+let searchObject;
+let searchObjectPath = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+let inputBox;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  imageMode(CENTER)
+  let button = createButton('show artwork');
+  button.position((windowWidth/2 - button.width/2), 50);
+  button.mousePressed(searchTerm);
+  inputBox = createInput(); 
+  inputBox.position((windowWidth/2 - inputBox.width/2), (25));
+}
+
+function searchTerm() {
+  let search = inputBox.value();
+  searchJSON = loadJSON(searchPath + search, searchContent);
+}
+
+function searchContent() {
+  if (searchJSON.total > 0) {
+    searchObject = random(searchJSON.objectIDs); 
+    objectJSON = loadJSON(searchObjectPath + searchObject, showContent);
+  } else {
+    console.log("No results were found at the met for: " + search);
+  }
+}
+
+function showContent() {
+  if (objectJSON.primaryImage == "") {
+    searchContent(); 
+  } else {
+    img = loadImage(objectJSON.primaryImage, drawImage);
+  }
+}
+
+function drawImage() {
+  clear();
+  background(220)
+  let aspect = img.width / img.height;
+  let scaledWidth = windowWidth / 2;
+  let scaledHeight = scaledWidth / aspect;
+  image(img, (windowWidth/2), (windowHeight/2), scaledWidth, scaledHeight);
+}
